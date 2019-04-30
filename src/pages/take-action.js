6dfc8e3570a23eb4958458b0eaa10b7ca1b75c88
Paul Roeland <paul@cleanclothes.org>
2019-04-30 12:42:36 +0200

    change take-action page

--- src/pages/take-action.js
+++ src/pages/take-action.js
@@ -7,63 +7,171 @@ import berlin01 from '../assets/images/berlin01.jpg'
 import wemove from '../assets/images/wemove.png'
 import challenge01 from '../assets/images/challenge01.jpg'
 import challenge02 from '../assets/images/challenge02.jpg'
+import { TwitterShareButton } from 'react-twitter-embed'
 
-const Generic = (props) => (
-    <Layout>
-        <Helmet>
-            <title>Take action - Turn H&M Around</title>
-            <meta name="description" content="Sign the petition and let your voice be heard" />
-        </Helmet>
+const Generic = props => (
+  <Layout>
+    <Helmet>
+      <title>Take action - Turn H&M Around</title>
+      <meta
+        name="description"
+        content="Sign the petition and let your voice be heard"
+      />
+    </Helmet>
 
-        <div id="main" className="alt">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h1>WILL YOU HELP TURN H&amp;M AROUND ?</h1>
-                    </header>
-                    <span className="image main"><img src={stockholm01} alt="" /></span>
-                    <p>In 2013 H&amp;M committed to ensuring living wages by 2018, which brought the brand a lot of positive media coverage. Now H&amp;M is trying to cover up that commitment, pretending they have been saying something else all along.</p>
-                    <p>Meanwhile hundreds of thousands of workers making H&amp;M clothing still cannot lift themselves out of poverty with the hard work hidden behind the glossy storefronts. </p>
-                    <p>H&amp;M has the financial means and the power to stay true to their original commitment. Not only that – they could even go beyond that and cover their whole supply chain. </p>
-                    <p>You have a voice in determining H&amp;M’s course of action! </p>
-                     <h3><a href="https://act.wemove.eu/campaigns/Living-Wages-HM" target="_blank" rel="noopener noreferrer">Sign the petition</a></h3>
-                    <a href="https://act.wemove.eu/campaigns/Living-Wages-HM" target="_blank" rel="noopener noreferrer"><img className="image right" src={wemove} alt="logo wemove.eu" /></a>
-                    <p>We've teamed up with <a href="https://act.wemove.eu/campaigns/Living-Wages-HM" target="_blank" rel="noopener noreferrer">wemove.eu</a> to let our voices be heard.</p>
-                    <p>So please <a href="https://act.wemove.eu/campaigns/Living-Wages-HM" target="_blank" rel="noopener noreferrer">sign the petition</a> and let your friends know to do the same!</p>
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h1>WILL YOU HELP TURN H&amp;M AROUND ?</h1>
+          </header>
+          <p>
+            In 2013 H&amp;M committed to ensuring living wages by 2018, which
+            brought the brand a lot of positive media coverage. Now H&amp;M is
+            trying to cover up that commitment, pretending they have been saying
+            something else all along. Meanwhile hundreds of thousands of workers
+            making H&amp;M clothing still cannot lift themselves out of poverty
+            with the hard work hidden behind the glossy storefronts. H&amp;M has
+            the financial means and the power to stay true to their original
+            commitment. Not only that – they could even go beyond that and cover
+            their whole supply chain.
+          </p>
+          <h3>Use your voice on social media</h3>
+          <p>
+            We are giving H&amp;M shareholders the opportunity to improve
+            millions of lives: by voting for our living wage resolution on 7 May
+            when they meet in Stockholm.{' '}
+          </p>
+          <p>
+            Your voice can make a difference! Ask key shareholders to use their
+            influence and do the right thing!
+          </p>
+          <h3>Twitter</h3>
+          <b>Tweet the following using the button:</b>
+          <p>
+            I support the @cleanclothes #LivingWage resolution and urge you to
+            vote for it on 7 May! Do the right thing @Swedbank @nordea @folksam
+            @Alecta @storebrand_no @AP4se #ShareYourProfits #TurnAroundHM
+            #LivingWageNow@AP4se
+          </p>
+          <TwitterShareButton
+            options={{
+              text:
+                'I support the @cleanclothes #LivingWage resolution and urge you to vote for it on 7 May! Do the right thing @Swedbank @nordea @folksam @Alecta @storebrand_no #ShareYourProfits @AP4se #TurnAroundHM #LivingWageNow',
+            }}
+          />
 
-                    <h3>Use your voice on social media</h3>
-
-                    <p>You can join the <b><a href="https://www.instagram.com/explore/tags/turnaroundhmchallenge/" target="_blank" rel="noopener noreferrer">#TurnaroundHMChallenge</a></b>, and have some fun on Instagram!</p>
-                    <h4>HOW?</h4>
-                    <img src={challenge01} alt="Instagam challenge" />  <img src={challenge02} alt="Instagam challenge" />
-                    <ul>
-                        <li><b>Sign the <a href="https://act.wemove.eu/campaigns/Living-Wages-HM" target="_blank" rel="noopener noreferrer">petition</a></b></li>
-                        <li><b>Take a picture</b> of something overturned, or turned around, or take a selfie and overturn it</li>
-                        <li><b>Post it on Instagram</b> using #turnaroundhm and #turnaroundhmchallenge and nominate two or more friends to do the same!</li>
-                    </ul>
-
-                    <p>For inspiration, have a look at <a href="https://www.instagram.com/explore/tags/turnaroundhmchallenge/" target="_blank" rel="noopener noreferrer">what others have done</a>. Have fun!</p>
-
-
-                    <div dangerouslySetInnerHTML={{ __html: '<a href="https://twitter.com/intent/tweet?button_hashtag=turnaroundhm&ref_src=twsrc%5Etfw" className="twitter-hashtag-button" data-text="@HM #TurnAroundHM #LivingWageNow" data-lang="en" data-dnt="true" data-show-count="false"><b>Tweet your own message to H&amp;M (@HM), using #TurnAroundHM and #LivingWageNow</b></a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>' }} />
-
-                    <span className="image left"><img src={berlin01} alt="Activists in Berlin" /></span>
-                    <p><br />You could also tweet one of these messages:</p>
-                    <blockquote>I want workers behind @HM clothes to be paid a living wage – as #HM promised they would be by 2018! #TurnAroundHM #LivingWageNow </blockquote>
-                    <blockquote>I want #LivingWageNow for workers in @HM supply chain! #TurnAroundHM - Stop turning your back on the living wage commitment! #HM </blockquote>
-                    <blockquote>Hey, @HM, you committed to making sure that workers are paid a #LivingWage by 2018. Make it happen, #HM! #TurnAroundHM #LivingWageNow </blockquote>
-
-                    <h4>Leave a message on <a href="https://www.facebook.com/hm/" target="_blank" rel="noopener noreferrer">H&amp;M's Facebook wall.</a></h4>
-                    <p>Let your creative sparks fly, or use one of these messages:</p>
-                    <blockquote>I want people who make the clothes I buy from you to be paid a living wage. Please H&amp;M, stop turning your back on the commitment you made in 2013 and make sure workers get a #LivingWageNow! #TurnAroundHM</blockquote>
-                    <blockquote>I was shocked to learn about poverty wages at your "gold" and "platinum" supplier factories. Those workers were covered by your commitment to a living wage by 2018! No more empty words, H&amp;M, I want workers to be paid a #LivingWageNow! #TurnAroundHM</blockquote>
-                    <blockquote>It is important for me to know that workers who make my clothes can have decent lives. To begin with, I expect you to stay true to the commitment that 850,000 garment workers will get a living wage by 2018. #TurnAroundHM #LivingWageNow</blockquote>
-
-                </div>
-            </section>
+          <h3>Facebook</h3>
+          <b>
+            Please copy the following message on the wall of major H&amp;M
+            shareholders:
+          </b>
+          <p>
+            Despite H&amp;M’s promise of a living wage by 2018, workers who make
+            #HM clothes are still expected to survive on poverty wages. Please
+            use your influence as a major H&amp;M shareholder to change this. On
+            7 May, #ShareYourProfits and vote for the Clean Clothes Campaign’s
+            proposal to establish a living wage fund for the workers!
+            #TurnAroundHM #LivingWageNow
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://www.facebook.com/folksam/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://facebook.com/folksam
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/Nordea/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://facebook.com/Nordea
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/storebrand.no/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://facebook.com/storebrand.no
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/swedbanksverige/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://facebook.com/swedbanksverige
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/Alecta.se/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://www.facebook.com/Alecta.se/
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/familjenerlingperssonsstiftelse/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://www.facebook.com/familjenerlingperssonsstiftelse/
+              </a>
+            </li>
+          </ul>
+          <h3>
+            <a
+              href="https://act.wemove.eu/campaigns/Living-Wages-HM"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Sign the petition
+            </a>
+          </h3>
+          <a
+            href="https://act.wemove.eu/campaigns/Living-Wages-HM"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="image right" src={wemove} alt="logo wemove.eu" />
+          </a>
+          <p>
+            We've teamed up with{' '}
+            <a
+              href="https://act.wemove.eu/campaigns/Living-Wages-HM"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              wemove.eu
+            </a>{' '}
+            to let our voices be heard.
+          </p>
+          <p>
+            So please{' '}
+            <a
+              href="https://act.wemove.eu/campaigns/Living-Wages-HM"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              sign the petition
+            </a>{' '}
+            and let your friends know to do the same!
+          </p>
         </div>
-
-    </Layout>
+      </section>
+    </div>
+  </Layout>
 )
 
 export default Generic
---
+++ src/posts/resolution.md
@@ -0,0 +1,35 @@
+---
+title: Will H&M shareholders finally steer H&M in the right direction?
+date: '2019-04-30T09:17:00.284Z'
+path: '/resolution/'
+---
+
+We bought a few H&M shares, just so that we could formally propose a living wage fund for the workers. Shareholders will vote on our living wage resolution at the Annual General Meeting taking place on 7 May. This is an opportunity for them to improve millions of lives! Read the resolution and tell the shareholders to do the right thing.
+
+<!-- end -->
+
+### Resolution under agenda item “Disposal of the company’s earnings”
+
+The Annual General Meeting calls on the Board of Directors to pay no dividend for this financial year.
+
+The company’s earnings shall instead be transferred into a special “living wage fund” aimed to finance the company’s efforts to increase wages of workers in H&M’s supply chain. The fund shall be in place until supplier wages have reached at least living wage levels, and are thus human rights compliant.
+
+### Justification:
+
+The H&M group has not yet met the requirement to ensure workers in its whole supply chain at least a living wage. Meeting this requirement represents an **essential human rights duty**, as laid out for example in the UN Guiding Principles on Business and Human Rights.
+
+H&M’s own wage data as well as third-party reports show that the **measures undertaken so far have not yet delivered a living wage** to workers in the supply chain. In important production countries like India, Turkey and Bangladesh, wages would need to increase significantly to secure workers and their families a life in dignity. In Bangladesh workers are demanding that the wages they are getting at H&M suppliers be at least doubled, so that they can afford decent housing, food and health care for themselves and their children.
+
+Not meeting the company’s human rights obligations also represents a **high risk for the future of the company**. The strikes over low wages in Bangladesh in January 2019 show how poverty wages lead to disruption of production. Consumers are also increasingly cautious about brands’ reputation when it comes to sustainability. A clear sign of this is that over 140,000 people signed Clean Clothes Campaign’s petition calling on H&M to keep the promise concerning living wages to garment workers.
+
+As responsible investors, it is our firm conviction that financial returns shall never be based on exploitation of labor. **Paying poverty wages to those who create the value added in the products sold by the H&M group is unacceptable**. The company’s earnings should therefore not be disbursed as dividend, but instead be used to build a “living wage fund”.
+
+The fund shall serve to:
+
+- **raise the wages** of workers in H&M’s supply chain who are earning poverty wages;
+- **pay higher prices to suppliers** and at the same time ring-fence the cost of labor at living wages level;
+- **put in place monitoring mechanisms** that ensure that suppliers pass on the increased prices to workers’s wages;
+- **fund living wage contributions** in joint initiatives with trade unions aiming at closing the living wage gap; and
+- **reorganize H&M’s supply chain** with living wages at the heart.
+
+The fund shall be maintained until wages at all levels of the supply chain are at least at living wage level and H&M’s earnings no more rest on poverty wages.
